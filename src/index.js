7014849176df578841f736bb3039cd1150916670
Aliaksandr Fedukovich <1632637@mail.ru>
2021-07-18 20:00:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let min = array[0];
+  for (let key of array) {
+    if (min > key) {
+      min = key;
+    }
+  }
+  return min;
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let max = array[0];
+  for (let key of array) {
+    if (max < key) {
+      max = key;
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let key of array) {
+    sum += key;
+  }
+  return (sum/array.length);
 }
